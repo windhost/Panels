1c8815b4bbeafbf59ea4d9318114d289ff130256
wind <wind@winds-MBP.lan>
2016-06-23 15:38:42 +0800

    without image

--- components/Panel.js
+++ components/Panel.js
@@ -1,109 +1,80 @@
+'use strict';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Navigator,
+  TouchableHighlight,
+  Animated,
+  Image
+} from 'react-native';
+import React, {
+  Component
+} from 'react';
 
-import React,{Component,StyleSheet,Text,View,Image,TouchableHighlight,Animated} from 'react-native';
 
-class Panel extends Component{
-    constructor(props){
-        super(props);
+export default class Panel extends Component {
+  constructor(props) {
+    super(props);
 
-        this.icons = {
-            'up'    : require('./images/Arrowhead-01-128.png'),
-            'down'  : require('./images/Arrowhead-Down-01-128.png')
-        };
+    this.state = {
+      title: props.title,
+      expanded: false,
+      animation: new Animated.Value()
+    };
+  }
 
-        this.state = {
-            title       : props.title,
-            expanded    : true,
-            animation   : new Animated.Value()
-        };
-    }
+  toggle() {
 
-    toggle(){
-        let initialValue    = this.state.expanded? this.state.maxHeight + this.state.minHeight : this.state.minHeight,
-            finalValue      = this.state.expanded? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
+    let initialValue = this.state.expanded ? this.state.maxHeight + this.state.minHeight : this.state.minHeight,
+      finalValue = this.state.expanded ? this.state.minHeight : this.state.maxHeight + this.state.minHeight;
 
-        this.setState({
-            expanded : !this.state.expanded
-        });
+    this.setState({
+      expanded: !this.state.expanded
+    });
 
-        this.state.animation.setValue(initialValue);
-        Animated.spring(
-            this.state.animation,
-            {
-                toValue: finalValue
-            }
-        ).start();
-    }
+    this.state.animation.setValue(initialValue);
+    Animated.spring(
+      this.state.animation, {
+        toValue: finalValue
+      }
+    ).start();
 
-    _setMaxHeight(event){
-        this.setState({
-            maxHeight   : event.nativeEvent.layout.height
-        });
-    }
+  }
 
-    _setMinHeight(event){
-        this.setState({
-            minHeight   : event.nativeEvent.layout.height
-        });
-    }
+  setMinHeight(event) {
+    this.setState({
+      minHeight: event.nativeEvent.layout.height
+    });
+  }
 
-    render(){
-        let icon = this.icons['down'];
+  setMaxHeight(event) {
+    this.setState({
+      maxHeight: event.nativeEvent.layout.height
+    });
+  }
 
-        if(this.state.expanded){
-            icon = this.icons['up'];
-        }
+  render() {
 
-        return (
-            <Animated.View 
-                style={[styles.container,{height: this.state.animation}]}>
-                <View style={styles.titleContainer} onLayout={this._setMinHeight.bind(this)}>
-                    <Text style={styles.title}>{this.state.title}</Text>
-                    <TouchableHighlight 
-                        style={styles.button} 
-                        onPress={this.toggle.bind(this)}
-                        underlayColor="#f1f1f1">
-                        <Image
-                            style={styles.buttonImage}
-                            source={icon}
-                        ></Image>
-                    </TouchableHighlight>
-                </View>
-                
-                <View style={styles.body} onLayout={this._setMaxHeight.bind(this)}>
-                    {this.props.children}
-                </View>
+    return (
+      <Animated.View style={[styles.container, {height: this.state.animation}]} >
+        <View style= {styles.headerContainer} onLayout={this.setMinHeight.bind(this)}> 
+          <TouchableHighlight style={styles.button} onPress={this.toggle.bind(this)} underlayColor={this.props.underlayColor}>
+            {this.props.header}
+          </TouchableHighlight>
+        </View>
 
-            </Animated.View>
-        );
-    }
+        <View style={styles.body} onLayout={this.setMaxHeight.bind(this)}>{this.props.content}</View>
+    </Animated.View>
+    );
+  }
+
 }
 
 var styles = StyleSheet.create({
-    container   : {
-        backgroundColor: '#fff',
-        margin:10,
-        overflow:'hidden'
-    },
-    titleContainer : {
-        flexDirection: 'row'
-    },
-    title       : {
-        flex    : 1,
-        padding : 10,
-        color   :'#2a2f43',
-        fontWeight:'bold'
-    },
-    button      : {
-
-    },
-    buttonImage : {
-        width   : 30,
-        height  : 25
-    },
-    body        : {
-        padding     : 10,
-        paddingTop  : 0
-    }
+  container: {},
+  headerContainer: {},
+  header: {},
+  body: {}
 });
-
-export default Panel;
